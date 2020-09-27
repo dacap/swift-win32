e63d48326682441a0ff215ac1acaa23988627151
Saleem Abdulrasool <compnerd@compnerd.org>
2020-09-27 13:11:03 -0700

    UI: improve view management of ViewController
    This lays out some of the basics for view management within the
    ViewController.  The missing piece is the presentation of the view
    associated with a view controller.

--- Sources/UI/ViewController.swift
+++ Sources/UI/ViewController.swift
@@ -7,12 +7,50 @@
 
 import WinSDK
 
-public class ViewController {
+public class ViewController: Responder {
   /// Managing the View
-  public var view: View!
 
-  public var preferredContentSize: Size { fatalError("not yet implemented") }
+  /// The view that the controller manages.
+  public var view: View! {
+    get {
+      loadViewIfNeeded()
+      return self.viewIfLoaded
+    }
+    set {
+      // disconnect the view controller from the previous view
+      self.viewIfLoaded?.viewController = nil
 
+      // attach the view and view controller
+      self.viewIfLoaded = newValue
+      self.viewIfLoaded?.viewController = self
+    }
+  }
+
+  /// The controller's view or `nil` if the view is not yet loaded.
+  public private(set) var viewIfLoaded: View?
+
+  /// Indicates if the view is loaded into memory.
+  public var isViewLoaded: Bool {
+    return self.viewIfLoaded == nil ? false : true
+  }
+
+  /// Creates the view that the controler manages.
+  public func loadView() {
+    // self.view = View(frame: .zero)
+  }
+
+  /// Called after the controller's view is loaded into memory.
+  public func viewDidLoad() {
+  }
+
+  /// Loads the controller's view if it has not yet been loaded.
+  public func loadViewIfNeeded() {
+    guard !self.isViewLoaded else { return }
+    self.loadView()
+    self.viewDidLoad()
+  }
+
+  /// A localized string that represents the view this controller manages.
   public var title: String? {
     get {
       let szLength: Int32 = GetWindowTextLengthW(view.hWnd)
@@ -36,7 +74,31 @@ public class ViewController {
     }
   }
 
-  public init() {
+  /// The preferred size for the view controller's view.
+  public var preferredContentSize: Size = .zero
+
+  public override init() {
+  }
+
+  /// Presenting a View Controller
+
+  /// Presents a view controller modally.
+  public func present(_ controller: ViewController, animated: Bool,
+                      completion: (() -> Void)? = nil) {
+  }
+
+  /// Getting Other Related View Controllers
+
+  /// The view controller that presented this view controller.
+  public private(set) var presentingViewController: ViewController?
+
+  /// The view controller that is presented by this view controller, or one of
+  /// its ancestors in the view controller hierarchy.
+  public private(set) var presentedViewController: ViewController?
+
+  // Responder Chain
+  public override var next: Responder? {
+    return view?.superview
   }
 }
 
